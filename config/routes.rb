5c0241447b9072253816c5e532879008c050027f
Eickichi <Davidye75019@gmail.com>
2019-12-03 16:55:28 +0100

    controllers done

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  root 'home_page#index'
+  resources :events
+  resources :games
+  resources :home_page
+  resources :users
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
