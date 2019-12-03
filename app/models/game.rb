c18f5e38c2eeb99d3ae02bd873bb0aaea541611c
Jawaad <Jawaadsaid@hotmail.fr>
2019-12-03 15:48:53 +0100

    database created

---
+++ app/models/game.rb
@@ -0,0 +1,9 @@
+class Game < ApplicationRecord
+
+    validates :game_name, presence:true
+    has_many :game_playeds 
+    has_many :users, through: :game_played
+    has_many :game_comments
+    has_many :users, through: :game_comment
+
+end
