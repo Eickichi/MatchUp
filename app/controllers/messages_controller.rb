99d05b17513e24a794e9882fbd30af6d9c7ee849
Pougliche <pugliese.viktor@gmail.com>
2019-12-05 20:19:58 +0100

    messagerie OK

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,4 @@
-class MessageController < ApplicationController
+class MessagesController < ApplicationController
     before_action do
         @conversation = Conversation.find(params[:conversation_id])
       end
