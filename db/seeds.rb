d36307b8a36f40bab15287cf0724e1611cf6cc72
Pougliche <pugliese.viktor@gmail.com>
2019-12-06 19:48:10 +0100

    modif seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,21 +3,21 @@ require 'faker'
 Event.delete_all
 user_count = User.all.count
 
-20.times do |f|
+5.times do |f|
         User.create!(user_name: Faker::Games::Pokemon.name, first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, bio: Faker::Quote.famous_last_words, email: "Sasuke#{f+user_count}@yopmail.com", password:"Helloworld", gamertag: Faker::Games::Pokemon.location, city: "Paris", social_tag: Faker::Games::SuperSmashBros.fighter, birth_date: Faker::Date.birthday(min_age: 18, max_age: 65) )
 end
 
 puts "20 utilisateurs crees"
 
 6.times do  
-        Event.create!(event_name: Faker::Cannabis.cannabinoid, description: Faker::ChuckNorris.fact, creator: User.all.sample, city: "Paris", duration: rand(1..10)*5, event_date: Faker::Date.forward(days: 100), game_played: Faker::Game.title)
+        Event.create!(event_name: Faker::JapaneseMedia::OnePiece.akuma_no_mi, description: Faker::ChuckNorris.fact, creator: User.all.sample, city: "Paris", duration: rand(1..10)*5, event_date: Faker::Date.forward(days: 100), game_played: Faker::Game.title)
 end
 
 puts "20 evenements crees"
 
-20.times do  
-    Game.create!(game_name: Faker::Game.title, platform: Faker::Game.platform, genre: Faker::Game.genre)
-end
+#20.times do  
+    #Game.create!(game_name: Faker::Game.title, platform: Faker::Game.platform, genre: Faker::Game.genre)
+#end
 
 
 20.times do		
@@ -30,7 +30,7 @@ end
 
 puts "20 commentaires de jeux & d'event crees"
 
-20.times do		
+60.times do		
 	GamePlayed.create!(user: User.all.sample, game: Game.all.sample)
 end
 
