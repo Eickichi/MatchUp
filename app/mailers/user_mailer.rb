ce625e6e2429094a4787055cc1a79062944aa647
Pougliche <pugliese.viktor@gmail.com>
2019-12-06 23:30:47 +0100

    mailer details

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,15 +1,15 @@
 class UserMailer < ApplicationMailer
-    default from: 'no-reply@monsite.fr'
+    default from: 'no-reply@smashup.com'
  
     def welcome_email(user)
       #on récupère l'instance user pour ensuite pouvoir la passer à la view en @user
       @user = user 
   
       #on définit une variable @url qu'on utilisera dans la view d’e-mail
-      @url  = 'http://monsite.fr/login' 
+      @url  = 'https://match-up-1.herokuapp.com/' 
   
       # c'est cet appel à mail() qui permet d'envoyer l’e-mail en définissant destinataire et sujet.
-      mail(to: @user.email, subject: 'Bienvenue chez nous !') 
+      mail(to: @user.email, subject: "L'aventure commence sur SmashUp !") 
     end
 
 end
