a85ee828ae71641ebfa08abb87ed3d10999bfa86
Pougliche <pugliese.viktor@gmail.com>
2019-12-06 20:23:17 +0100

    seed final

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,14 +3,14 @@ require 'faker'
 Event.delete_all
 user_count = User.all.count
 
-5.times do |f|
-        User.create!(user_name: Faker::Games::Pokemon.name, first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, bio: Faker::Quote.famous_last_words, email: "Sasuke#{f+user_count}@yopmail.com", password:"Helloworld", gamertag: Faker::Games::Pokemon.location, city: "Paris", social_tag: Faker::Games::SuperSmashBros.fighter, birth_date: Faker::Date.birthday(min_age: 18, max_age: 65) )
-end
+#5.times do |f|
+        #User.create!(user_name: Faker::Games::Pokemon.name, first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, bio: Faker::Quote.famous_last_words, email: "Sasuke#{f+user_count}@yopmail.com", password:"Helloworld", gamertag: Faker::Games::Pokemon.location, city: "Paris", social_tag: Faker::Games::SuperSmashBros.fighter, birth_date: Faker::Date.birthday(min_age: 18, max_age: 65) )
+#end
 
 puts "20 utilisateurs crees"
 
 6.times do  
-        Event.create!(event_name: Faker::JapaneseMedia::OnePiece.akuma_no_mi, description: Faker::ChuckNorris.fact, creator: User.all.sample, city: "Paris", duration: rand(1..10)*5, event_date: Faker::Date.forward(days: 100), game_played: Game.all.sample)
+        Event.create!(event_name: Faker::JapaneseMedia::OnePiece.akuma_no_mi, description: Faker::ChuckNorris.fact, creator: User.all.sample, city: "Paris", duration: rand(1..10)*5, event_date: Faker::Date.forward(days: 100), game_played: Game.all.sample.game_name)
 end
 
 puts "20 evenements crees"
