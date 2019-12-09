4bf01c64d7d5b91022dff6440af5b1a5e4901a89
Eickichi <Davidye75019@gmail.com>
2019-12-09 19:16:46 +0100

    active_storage event bug fixed

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema.define(version: 2019_12_06_154503) do
-=======
 ActiveRecord::Schema.define(version: 2019_12_06_144939) do
->>>>>>> developpement
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
