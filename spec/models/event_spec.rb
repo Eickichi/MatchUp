40896e67e9dc4a2d56bf64917aa12daa4da640bf
Batmams <mamadou.koita@gmail.com>
2019-12-12 18:31:37 +0100

    ignore env files

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -16,30 +16,28 @@ RSpec.describe Event, type: :model do
              { expect(@event).to validate_presence_of(:duration) }
          end
 
-         it"validate time_gate" do
+         it "validate time_gate" do
              { expect(@event).to validate(:time_gate) }
          end
 
-         it"validate event_name" do
-            { expect(@event).to validate_lenght_of(:event_name).is_at_least(5) }
+         it "validate event_name" do
+             expect(@event).to validate_lenght_of(:event_name).is_at_least(5) 
          end
          
-         it"validate city" do
-             { expect(@event).to validate_presence_of(:city) }
+         it "validate city" do
+              expect(@event).to validate_presence_of(:city) 
          end
 
          it "validate game_played" do
-
-             { expect(@event).to validate_presence_of(:game_played) }
+              expect(@event).to validate_presence_of(:game_played) 
          end
          
         end
 
         context "associations" do
 
-            describe"attendances" do
+            describe "attendances" do
                 
             end
 
-         end
-   end  
+end
--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -4,9 +4,9 @@ RSpec.describe User, type: :model do
 
     context "validation" do
 
-        it"validate game_name" do
+        it "validate game_name" do
 
-            it{expect(@game).to validate_presence_of(:game_name)}
+            it { expect(@game).to validate_presence_of(:game_name) }
 
         end
         
@@ -15,16 +15,16 @@ RSpec.describe User, type: :model do
     context "associations" do
 
         describe "game_playeds" do
-            it{expect(@game).to have_many(:game_playeds)}
+            it { expect(@game).to have_many(:game_playeds) }
         end
 
         describe "users" do
-            it{expect(@game).to have_many(:users)through(:game_playeds)}
-            it{expect(@game).to have_many(:users)through(:game_comments)}
+            it { expect(@game).to have_many(:users)through(:game_playeds) }
+            it { expect(@game).to have_many(:users)through(:game_comments) }
         end
 
         describe "game_comments" do
-            it{expect(@game).to have_many(:game_comments)}
+            it { expect(@game).to have_many(:game_comments) }
         end
 
     end
